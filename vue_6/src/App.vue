fdf5f0a865875b79c2c0e254b5c14ec714f2df2f
ropeJIbI4 <pogi199411@gmail.com>
2024-05-26 00:32:03 +0300

    first

---
+++ vue_6/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Index from './components/Index.vue';
+import Blog from './components/Blog.vue';
+import BlogDetails from './components/BlogDetails.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import ProductDetails from './components/ProductDetails.vue';
+import OurProject from './components/OurProject.vue';
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <Header />
+    <!-- <Index/> -->
+    <!-- <Blog/> -->
+    <!-- <BlogDetails /> -->
+    <!-- <ProductDetails/> -->
+    <OurProject/>
+    <Footer/>
+  </main>
+</template>
+
+
+<style>
+
+</style>
+
+
+
+<style scoped>
+</style>
